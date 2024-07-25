1075162e75a68532b39a3685e13b0c1ef580fa32
jianfengtheboy <jianfengtheboy@163.com>
2024-07-25 23:26:44 +0800

    chore(更新项目模版): 更新项目模版
    更新项目模版

--- packages/init.js
+++ packages/init.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Sun
+ * @LastEditors: Sun
+ * @Email: jianfengtheboy@163.com
+ * @Date: 2021-12-03 17:32:48
+ * @LastEditTime: 2024-07-25 23:17:43
+ * @Description: 初始化
+ */
 const { program } = require('commander')
 const { log } = require('./lib/utils')
 
@@ -10,7 +18,7 @@ program
   .action(require('./commands/create'))
   .on('-h, --help', () => {
     log('使用 sim create NAME -t=TYPE 进行初始化操作')
-    log('NAME 名称 规则：sun-web-类型-名称，例如：sun-web-app-ui 移动端，sun-web-component-table table组件')
+    log('NAME 名称 规则：sun-web-类型-名称，例如：sun-web-app-ui 移动端')
     log('TYPE 类型 规则：')
     log('vue: vue项目')
     log('component: 组件')
